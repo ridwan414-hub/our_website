3b4a93d0d15b7484b67bbc8a988862458255853d
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-05 17:49:40 +0600

    only ridwan's main folder added

--- main/src/main.jsx
+++ main/src/main.jsx
@@ -6,11 +6,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage.jsx';
 import App from './App.jsx';
 import Contact from './pages/Contact.jsx';
-import Reviews from './pages/Reviews.jsx';
+// import Reviews from './pages/Reviews.jsx';
 import Videos from './pages/Videos.jsx';
 import Products from './pages/Products.jsx';
 import Blogs from './pages/Blogs.jsx';
-import Login from './pages/Login.jsx';
+// import Login from './pages/Login.jsx';
 import Carts from './pages/Carts.jsx';
 
 const router = createBrowserRouter([
@@ -24,11 +24,11 @@ const router = createBrowserRouter([
     element: <Products></Products>,
     errorElement: <ErrorPage></ErrorPage>,
   },
-  {
-    path: '/reviews',
-    element: <Reviews></Reviews>,
-    errorElement: <ErrorPage></ErrorPage>,
-  },
+  // {
+  //   path: '/reviews',
+  //   element: <Reviews></Reviews>,
+  //   errorElement: <ErrorPage></ErrorPage>,
+  // },
   {
     path: '/videos',
     element: <Videos></Videos>,
@@ -44,11 +44,11 @@ const router = createBrowserRouter([
     element: <Contact></Contact>,
     errorElement: <ErrorPage></ErrorPage>,
   },
-  {
-    path: '/login',
-    element: <Login></Login>,
-    errorElement: <ErrorPage></ErrorPage>,
-  },
+  // {
+  //   path: '/login',
+  //   element: <Login></Login>,
+  //   errorElement: <ErrorPage></ErrorPage>,
+  // },
   {
     path: '/cart',
     element: <Carts></Carts>,
